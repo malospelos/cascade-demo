862624447d9af818362fa24a93e2c2eabec19ce3
Malospelos <cjfernanp@gmail.com>
2026-08-08 12:20:04 +0200

    Acreditar 5000 medallas en pack demo

--- stable-v24-demo-preload.js
+++ stable-v24-demo-preload.js
@@ -9,7 +9,7 @@ if(!s||s.v!==1){localStorage.removeItem(PENDING);return}
 s.powers=Object.assign({sonar:0,depth:0,hint:0,bombard:0},s.powers||{});
 if(p.pack==='torps')s.ammo=(s.ammo||0)+10;
 else if(p.pack==='tactical'){s.ammo=(s.ammo||0)+5;s.powers.sonar++;s.powers.hint++;}
-else if(p.pack==='medals')s.coins=(s.coins||0)+1000;
+else if(p.pack==='medals')s.coins=(s.coins||0)+5000;
 else if(p.pack==='full'){s.ammo=(s.ammo||0)+10;s.powers.sonar++;s.powers.depth++;s.powers.hint++;s.powers.bombard++;}
 localStorage.setItem(SAVE,JSON.stringify(s));
 localStorage.removeItem(PENDING);
